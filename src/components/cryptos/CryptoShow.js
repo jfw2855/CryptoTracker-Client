dde0d08fe5eceb99a201c47b897d550b4a0b5c9a
mitchgemma <mitchgemma@gmail.com>
2022-04-24 19:31:26 -0400

    adds stying, formatting, and more data

--- src/components/cryptos/CryptoShow.js
+++ src/components/cryptos/CryptoShow.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { showCrypto } from '../../api/cryptos'
 import { useParams, useNavigate } from 'react-router-dom'
-import { Spinner } from 'react-bootstrap'
+import { Spinner, Row, Col, Accordion } from 'react-bootstrap'
 
 const CryptoShow = () => {
 
@@ -27,14 +27,40 @@ const CryptoShow = () => {
     } 
 
   return (
-    <>
-      <h1 className='logo'>{crypto.name}</h1>
-      <p>${crypto.market_data.current_price.usd.toFixed(2)}  <span>24 hour change {crypto.market_data.price_change_percentage_24h.toFixed(2)}%</span></p>
-      <p>Market Cap: { crypto.market_data.market_cap.usd.toLocaleString() }</p>
-      {/* <p className='logo'><a>Test</a>{crypto.description.en}</p> */}
-      <td dangerouslySetInnerHTML={{__html: crypto.description.en}} />
-      <a>Test</a>
-    </>
+   
+    <div className="container">
+      <Row>
+        <Col>
+          <h1 className='logo name'>{crypto.name}</h1><h6>{crypto.symbol.toUpperCase()}</h6>
+          <img src={crypto.image.small} alt="" /><br />
+          <span>Rank #{ crypto.market_cap_rank}</span>
+        </Col>
+        <Col>
+          <span>{ crypto.name } ({crypto.symbol}) price</span> <br/>
+          <h1 className='price'>${crypto.market_data.current_price.usd.toLocaleString(undefined, { maximumFractionDigits: 2 })}</h1> <br/>
+          <Row>
+            <Col>
+              <span>24hr change </span><br />
+              <h1>
+              {crypto.market_data.price_change_percentage_24h.toFixed(2)}%
+              </h1>
+            </Col>
+            <Col>
+              <span>Market Cap</span><br />
+              <h1>
+              {crypto.market_data.market_cap.usd.toLocaleString()}
+              </h1>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Accordion>
+        <Accordion.Header>Want to learn more about {crypto.name}?</Accordion.Header>
+        <Accordion.Body>
+         <td dangerouslySetInnerHTML={{__html: crypto.description.en}} />
+        </Accordion.Body>
+      </Accordion>
+    </div>
   )
 }
 
