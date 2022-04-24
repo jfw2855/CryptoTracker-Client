a66f970b934114bcc6e14532d730f553829eaeb5
jfw2855 <jwaddle1130@gmail.com>
2022-04-24 18:46:09 -0500

    calls external api with server response to get assets market data

--- src/components/portfolio/PortfolioIndex.js
+++ src/components/portfolio/PortfolioIndex.js
@@ -1,3 +1,4 @@
+import { queryByAltText } from '@testing-library/react'
 import React, { useState, useEffect, useCallback } from 'react'
 import { Form, Container, Button, Card, Link, Row, Col, ListGroup } from 'react-bootstrap'
 import { viewPortfolio, getPData } from '../../api/portfolio'
@@ -6,37 +7,37 @@ import { viewPortfolio, getPData } from '../../api/portfolio'
 const PortfolioIndex = (props) => {
 
   const [assets,setAssets] = useState(null)
+  // mData == Market Data -> state var to set current market data of user's assets
+  const [mData,setMData] = useState(null)
   const { user,msgAlert} = props
   let assetsDisplay = null
   let query = ""
 
-  
-  useEffect(() => {
-    viewPortfolio(user)
-      .then((res) => {
-        setAssets(res.data.portfolio[0].assets)
-        //console.log('this IS PORTFOLIO', res.data.portfolio)
-      })
-      .then(()=>{
-        for (let i in assets) {
-          console.log('coin! in assets!',assets[i])
-          query+=`${assets[i].coinGeckId}%2C`
-        }
-        console.log('this is query!',query)
-        getPData(query)
-         .then((test)=> {
-           console.log('this is the res from get crypts',test.data)
 
-         })
-      })
-      .catch((err) => console.log(err))
+  // calls backend server for portfolio assets then uses info to make an external api call to grab market data
+  useEffect(() => {
+    const fetchData = async () => {
+      const respAssets = await viewPortfolio(user)
+      const respCoins = respAssets.data.portfolio[0].assets
+
+      for (let i in respCoins) {
+        query+=`${respCoins[i].coinGeckId}%2C`
+      }
+      const respMData = await getPData(query)
+      console.log('querryyy data',respMData.data)
+
+      // updates the response data with average price and quantity of assets
+      for (let i in respMData.data) {
+        respMData.data[i].avgPrice = respCoins[i].avgPrice
+        respMData.data[i].quantity = respCoins[i].quantity
+      }
+      console.log('fixed data!!!!!!!!',respMData.data)
+    }
+    fetchData()
   }, [])
   
-  if(!assets) {
+  if(!mData) {
     return <p>Loading...</p>
-  } else {
-    
-    console.log('portfolio!!!!',assets)
   }
 
 
