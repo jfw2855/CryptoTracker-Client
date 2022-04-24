94087a372a74648bfe505b8749ee43c606819eb0
mitchgemma <mitchgemma@gmail.com>
2022-04-24 15:40:44 -0400

    adds crypto show page

--- src/api/cryptos.js
+++ src/api/cryptos.js
@@ -5,11 +5,24 @@ import axios from 'axios'
 // GET top xxx cryptos
 export const getCryptos = () => {
   const searchUrl =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false'
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
   const config = {
     method: 'get',
     url: `${searchUrl}`,
   }
-  //   console.log('this is config', config)
   return axios(config)
 }
+
+// SHOW a single crypto
+export const showCrypto = (coin) => {
+  const searchUrl = `https://api.coingecko.com/api/v3/coins/${coin}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=true&sparkline=true`
+  const config = {
+    method: 'get',
+    url: `${searchUrl}`,
+  }
+  return axios(config)
+
+}
+
+
+// https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=true&developer_data=true&sparkline=true
