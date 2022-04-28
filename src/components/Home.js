9b781532daa115e0b16c4e9383e663e0fb3aa7dd
mitchgemma <mitchgemma@gmail.com>
2022-04-28 19:41:44 -0400

    adds props to be sent to cryptos index page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,12 @@ import CryptosIndex from './cryptos/CryptosIndex'
 const Home = (props) => {
   // const { msgAlert, user } = props
   //   console.log('props in home', props)
-
+const {user} = props
   return (
     <>
-      <CryptosIndex />
+      <CryptosIndex
+      user = {user}
+      />
     </>
   )
 }
