ae98d6205ded59b9089987a73eea2a4548dea6cb
jfw2855 <jwaddle1130@gmail.com>
2022-04-29 10:23:31 -0500

    creates newsfeed component

---
+++ src/components/Newsfeed.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+//import { getCryptoNews } from "../api/cryptos"
+
+const Newsfeed = () => {
+    // useEffect(()=>{
+    //     getCryptoNews()
+    //         .then(res=>{
+    //             console.log('this is the res',res)
+    //         })
+    // })
+    
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Newsfeed
